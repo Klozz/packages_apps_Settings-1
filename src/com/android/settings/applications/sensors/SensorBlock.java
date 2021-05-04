AOSiP/platform_packages_apps_Settings

Settings: Utilize our AppListPreference with Sensor Blocking pref
See https://review.aosip.dev/c/AOSIP/platform_vendor_aosip/+/17437

Change-Id: I0afe40cc0983766cbf727bfa67aea4e5285b2090

src/com/android/settings/applications/sensors/SensorBlock.java
@@ -42,6 +42,7 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.widget.FooterPreference;
 
+import com.aosip.support.preference.AppListPreference;
 import com.aosip.support.preference.PackageListAdapter;
 import com.aosip.support.preference.PackageListAdapter.PackageItem;
 
@@ -238,13 +239,12 @@ public class SensorBlock extends SettingsPreferenceFragment
         PackageInfo info = mPackageManager.getPackageInfo(pkg.name,
                 PackageManager.GET_META_DATA);
         Preference pref =
-                new Preference(getActivity());
+                new AppListPreference(getActivity());
 
         pref.setKey(pkg.name);
         pref.setTitle(info.applicationInfo.loadLabel(mPackageManager));
         pref.setIcon(info.applicationInfo.loadIcon(mPackageManager));
         pref.setPersistent(false);
-        pref.setLayoutResource(R.layout.custom_app_filter_list_view);
         pref.setOnPreferenceClickListener(this);
         return pref;
     }

Klozz/packages_apps_Settings-1

src/com/android/settings/applications/sensors/SensorBlock.java
@@ -42,6 +42,7 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.widget.FooterPreference;
 
+import com.derp.support.preference.AppListPreference;
 import com.derp.support.preference.PackageListAdapter;
 import com.derp.support.preference.PackageListAdapter.PackageItem;
 
@@ -238,13 +239,12 @@ public class SensorBlock extends SettingsPreferenceFragment
         PackageInfo info = mPackageManager.getPackageInfo(pkg.name,
                 PackageManager.GET_META_DATA);
         Preference pref =
-                new Preference(getActivity());
+                new AppListPreference(getActivity());
 
         pref.setKey(pkg.name);
         pref.setTitle(info.applicationInfo.loadLabel(mPackageManager));
         pref.setIcon(info.applicationInfo.loadIcon(mPackageManager));
         pref.setPersistent(false);
-        pref.setLayoutResource(R.layout.custom_app_filter_list_view);
         pref.setOnPreferenceClickListener(this);
         return pref;
     }